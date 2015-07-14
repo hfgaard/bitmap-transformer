1c31baa1adaa8a65d9d635b5fcca762ffb23dd4a
Hana Fallisgaard <hfgaard@gmail.com>
2015-07-13 17:58:03 -0700

    remove unused function

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 "use strict";
 var bitmapReader = require('./bitmapReader');
-var os = require('os');
-
-function userEndianness() {
-  if (os.endianness() === 'LE') {
-
-  }
-}
 
 bitmapReader('./bitmaps/non-palette-bitmap.bmp', './bitmaps/non-palette-bitmap-invert.bmp', 'invert');
 bitmapReader('./bitmaps/palette-bitmap.bmp', './bitmaps/palette-bitmap-invert.bmp', 'invert');
